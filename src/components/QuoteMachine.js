e88ea0237b8950fd608b1c127fdc32da6129f620
Jasper Alblas <just_jasper@hotmail.com>
2018-12-14 12:57:58 +0100

    Adds bootstrap styling, and eslint support

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,14 +1,14 @@
 import React, {Component}  from "react";
 
 class QuoteMachine extends Component {
-  
+
   render() {
     return(
       <div id="quote-box">
         <p id="text">{this.props.quote}</p>
         <p id="author">{this.props.author}</p>
         <a id="tweet-quote" href={"http://twitter.com/intent/tweet?text=" + this.props.quote}>Tweet quote</a>
-        <button id="new-quote" onClick={this.props.handler}>Random new quote</button>
+        <button id="new-quote" class="btn btn-light" onClick={this.props.handler}>Random new quote</button>
       </div>
     );
   }
